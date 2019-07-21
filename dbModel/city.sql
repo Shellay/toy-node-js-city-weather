26f076b21c68f3d4b0d46d63af567e74321f5fa0
Shellay <shellaylee@hotmail.com>
2019-07-21 17:19:12 +0200

    feature/3: use proper GIST index on table city

--- dbModel/city.sql
+++ dbModel/city.sql
@@ -5,4 +5,14 @@ CREATE TABLE city (
   coord GEOGRAPHY(POINT, 4326) NOT NULL
 );
 
-CREATE INDEX city_coord_gix ON city USING GIST(coord);
+/*
+NOTE: Index should be properly created for fast spatial queries
+  and needs to be verified via checking execution plan.
+(example src: https://gis.stackexchange.com/questions/198789/st-dwithin-why-does-query-not-use-the-spatial-index)
+*/
+CREATE INDEX gist_city_coord ON city USING GIST( (coord::GEOGRAPHY) );
+
+/*
+With the index above, execution plan should tell usage of Bitmap Index Scan
+on gist_city_coord.
+*/
