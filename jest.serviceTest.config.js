a3658f5ba350d22df1e7728eae01570bd0a40aba
Shellay <shellaylee@hotmail.com>
2019-07-21 15:24:23 +0200

    feature/2: adjust naming of service tests

--- jest.serviceTest.config.js
+++ jest.serviceTest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  testRegex: '.+\\.dbTest\\.js',
+  testRegex: '.+\\.serviceTest\\.js',
   globalSetup: '<rootDir>tests/testDbSetup.js',
   globalTeardown: '<rootDir>tests/testDbTeardown.js',
 }
